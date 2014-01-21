a6280a70d79e20a191c4217b3b3be1ae223bbc3a
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-01-21 14:11:35 -0800

    Add blog post functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,16 @@ class BlogsController < ApplicationController
 	before_action :signed_in_admin, only: [:new]
 
 	def new
+		@blog = Blog.new
+	end
+	
+	def create
+		@blog = Blog.new(blog_params)
+		if @blog.save
+			redirect_to blogs_url
+		else 
+			render 'new'
+		end
 	end
 	
 	def index
@@ -10,4 +20,16 @@ class BlogsController < ApplicationController
 	
 	def show 
 	end
+	
+	private
+	
+		def signed_in_admin
+			unless session[:admin_id]
+				redirect_to :back
+			end
+		end
+		
+		def blog_params
+			params.require(:blog).permit(:title,:contents, :tags)
+		end
 end
--- spec/requests/blog_pages_spec.rb
+++ spec/requests/blog_pages_spec.rb
@@ -5,6 +5,8 @@ describe "Blog" do
 	
 	let!(:blog) { Blog.create(contents: "Larem ipsum crossed the road",
 														title: "Larem ipsum", tags:"[comedy, latin]") }
+	let(:admin) { Admin.create(username:"Jim", password:"foobar", 
+														password_confirmation: "foobar") }
 	
 	
 	describe "blog page" do
@@ -16,4 +18,30 @@ describe "Blog" do
 		it { should have_content("comedy") }
 		it { should have_content("latin") }
 	end
+	
+	describe "signing as admin" do	
+		before do
+			visit signin_path
+			fill_in 'Username', with: admin.username
+			fill_in 'Password', with: admin.password
+			click_button 'Log in'
+		end
+		
+		it { should_not have_content('Sign in') }
+		
+		describe "creating new blog post" do
+			before do
+				click_link 'New Post'
+				fill_in 'Contents', with: "There is a large American heritage dictionary"
+				fill_in 'Title', with: "American Heritage Dictionary"
+				fill_in 'Tags', with: "dictionary, reference"
+				click_button 'Create Blog'
+			end
+			
+			it { should have_content("There is a large American heritage dictionary") }
+			it { should have_content("American Heritage Dictionary") }
+			it { should have_content("dictionary, reference") }
+		end
+	end		
+		
 end
