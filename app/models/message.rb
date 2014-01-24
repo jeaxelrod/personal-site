bbee24b981f6cc07ed30a25127c7f5b566f4e613
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-01-24 11:23:33 -0800

    Add footer and contact changes

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message
 	
 	attr_accessor :email, :name, :body, :subject
 	
-	validates_presence_of :name, :email, :subject, :body
+	validates_presence_of  :email,:body
 	validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 	
 end
