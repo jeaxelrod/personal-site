e44eab984a3869f19450640b755028242ce69a9a
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-02-24 20:32:37 -0800

    Typography header changes and general layout

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module PersonalApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
 		address:              'smtp.gmail.com',
